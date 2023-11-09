488f77141410e42f7d388ecf8b87247fa9042cbd
Anubhav Kumar Ray <64881373+anubhavray678@users.noreply.github.com>
2023-11-09 17:27:18 +0530

    Time: 2 ms (60.32%), Space: 6.8 MB (64.77%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        vector<bool> alpha(26,0);
+        for(int i = 0; i<s.size(); ++i)
+        {
+            alpha[s[i]-'a'] =1;
+        };
+        for(int i = 0; i<26; ++i){
+            if(alpha[i] == 0){
+                return 0;
+            }
+        };
+
+        return 1;
+    }
+};
