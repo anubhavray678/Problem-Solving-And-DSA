774e5da99f7c3436155b8f3f30f16f7897b2df37
Anubhav Kumar Ray <anubhavray678@gmail.com>
2024-11-03 18:31:21 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.81%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -5,7 +5,7 @@ public:
         char c = s[0];
         int index = 1;
         while(index<s.size()){
-            s[index-1] =s[index];
+            s[index-1] = s[index];
             index++;
         }
         s[s.size()-1] =c;
@@ -19,12 +19,9 @@ public:
             index--;
         }
         s[0] =c;
-
     }
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size())return false;
-        string clockwise,anticlockwise;
-        // clockwise=s;
         for(int i =0 ; i<s.size(); ++i){
             rotateclockwise(s);
             if(s == goal){
